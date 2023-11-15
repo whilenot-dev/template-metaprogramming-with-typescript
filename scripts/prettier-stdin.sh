c16ff0a1d9db992cd7a2d1a48c1001fcaa9cf70f
whilenot <git@whilenot.dev>
2023-11-15 13:30:51 +0100

    suffix arg for prettier-stdin

--- scripts/prettier-stdin.sh
+++ scripts/prettier-stdin.sh
@@ -3,6 +3,9 @@
 set -euo pipefail
 shopt -s expand_aliases
 
+# shellcheck disable=SC2016
+SUFFIX="${1:?'Suffix not set'}"
+
 DIRNAME=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 PROJECT_DIR=$( realpath "${DIRNAME}/.." )
 NODE_MODULES_BIN=$( realpath "${PROJECT_DIR}/node_modules/.bin" )
@@ -10,7 +13,7 @@ NODE_MODULES_BIN=$( realpath "${PROJECT_DIR}/node_modules/.bin" )
 # shellcheck disable=SC2139
 alias prettier="${NODE_MODULES_BIN}/prettier --config '${PROJECT_DIR}/.prettierrc'"
 
-TMP_FILE=$( mktemp --suffix=.ts )
+TMP_FILE=$( mktemp --suffix="${SUFFIX}" )
 trap cleanup 1 2 3 6 exit
 cleanup() {
     rm "${TMP_FILE}"
